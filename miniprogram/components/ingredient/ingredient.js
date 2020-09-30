c43b5e83a571c0c39ee00be7b0eeea026496a925
hzwdachui <wangdachui2014@hotmail.com>
2020-09-30 17:26:11 +0800

    fea/get ingredients

--- miniprogram/components/ingredient/ingredient.js
+++ miniprogram/components/ingredient/ingredient.js
@@ -18,6 +18,18 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    async getIngredientList() {
+      let that = this;
+      // console.log("[DEBUG] that.properties.item_id: ");
+      console.log(that.properties);
+      console.log("[DEBUG] that.properties.item_id: " + that.properties.item_id)
+      const { data: initData } = await DB.collection("items").doc(this.properties.item_id).get();
+      console.log('[DEBUG] init query ', initData)
+      this.setData({
+        item_id: initData.item_id,
+        img_src: initData.img_src,
+        item_name: initData.item_name
+      });
+    }
   }
 })
--- miniprogram/components/item/item.js
+++ miniprogram/components/item/item.js
@@ -4,9 +4,10 @@ const DB = wx.cloud.database()
 Component({
   lifetimes: {
     attached: function () {
-      setTimeout(()=>{
+      // this.getItem();
+      setTimeout(() => {
         this.getItem();
-      }, 500);
+      });
     }
   },
   /**
@@ -20,9 +21,11 @@ Component({
    * 组件的初始数据
    */
   data: {
-    item_id:"",
+    item_id: "",
     img_src: "",
-    item_name: ""
+    item_name: "",
+    // ingredients: [{name: "test_name1", category: "test_cat1"}]
+    ingredients: []
   },
 
   /**
@@ -33,14 +36,16 @@ Component({
       let that = this;
       // console.log("[DEBUG] that.properties.item_id: ");
       console.log(that.properties);
-      console.log("[DEBUG] that.properties.item_id: "+ that.properties.item_id)
+      console.log("[DEBUG] that.properties.item_id: " + that.properties.item_id)
       const { data: initData } = await DB.collection("items").doc(this.properties.item_id).get();
       console.log('[DEBUG] init query ', initData)
       this.setData({
         item_id: initData.item_id,
         img_src: initData.img_src,
-        item_name: initData.item_name
+        item_name: initData.item_name,
+        ingredients: initData.ingredients
       });
+      console.log('[DEBUG] init data ', this.data);
     }
   }
 })

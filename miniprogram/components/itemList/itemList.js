fdc9c426cb5795463dd9c288c8ece873a7e6b415
hzwdachui <wangdachui2014@hotmail.com>
2020-10-11 10:53:17 +0800

    fea/frontend search

--- miniprogram/components/itemList/itemList.js
+++ miniprogram/components/itemList/itemList.js
@@ -14,7 +14,10 @@ Component({
       this.getItemList();
       console.log("[DEBUG]")
       console.log(this);
-      console.log(this.data.itemList); // why it's empty???
+      console.log(this.data.itemList); // why it's empty???;
+      this.setData({
+        search: this.search.bind(this)
+      });
     }
   },
   /**
@@ -28,7 +31,8 @@ Component({
    * 组件的初始数据
    */
   data: {
-    itemList: []
+    itemList: [],
+    showItemList: []
   },
 
   /**
@@ -39,9 +43,19 @@ Component({
       const { data: initList } = await DB.collection("items").get();
       console.log('[DEBUG] init query item list', initList)
       this.setData({
-        itemList: initList
+        itemList: initList,
+        showItemList: initList
       });
+    },
+    search(value) {
+      // 输入过程不断调用此函数得到新的搜索结果，参数是输入框的值value，返回Promise实例
+      return new Promise((resolve, reject) => {
+        this.setData({
+          showItemList: this.data.itemList.filter((element) => {
+            return element.item_name.indexOf(value) !== -1;
+          })
+        });
+      })
     }
   }
-
 })
